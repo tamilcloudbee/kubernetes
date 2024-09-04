92cacc5a61da60eb46c2263cc1970f2e19967749
TamilCloudbee <tamilcloudbee@gmail.com>
2024-09-04 16:03:20 +0530

    added scripts/kubernetes-install.sh

--- scripts/kubernetes-install.sh
+++ scripts/kubernetes-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
+K8S_VERSION="v1.30"
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/k8s.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$K8S_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/k8s.gpg
 
-echo 'deb [signed-by=/etc/apt/keyrings/k8s.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/k8s.list
+echo "deb [signed-by=/etc/apt/keyrings/k8s.gpg] https://pkgs.k8s.io/core:/stable:/$K8S_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/k8s.list
 
 sudo apt update && sudo apt install kubelet kubeadm kubectl -y
-
 
